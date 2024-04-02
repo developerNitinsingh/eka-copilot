0328039c2e2c2aaf7fd80eaf0f158d6c27ea069c
developerNitinsingh <nitinsingh09871@gmail.com>
2024-04-02 22:06:56 +0530

    return back in before version

--- src/App.jsx
+++ src/App.jsx
@@ -48,8 +48,8 @@ function App() {
 
       <BrowserRouter>
         <Routes>
-          <Route path="/page" element={<Home />} />
-          <Route path="/" element={<Page00 />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/page" element={<Page00 />} />
           <Route path="/request-started-01" element={<RequestStarted01 />} />
           <Route path="/overview" element={<OverView />} />
           <Route path="/request-started-1" element={<RequestStarted1 />} />
